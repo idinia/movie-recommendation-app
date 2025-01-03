f885f0f19540e3cb85263e21cf5c7c8a4e8940bd
Izzy <ismail.dinia@gmail.com>
2025-01-03 18:39:03 +0100

    added variables and details about favorite movies

--- basic.py
+++ basic.py
@@ -1,1 +1,9 @@
 print(" Welcome to this kickass movie recommendation app")
+
+favorite_movie = "The Shawshank Redemption"
+genre = "Drama"
+release_year = 1994
+duration_in_minutes = 142
+imdb_rating = 9.3
+
+print(f" My favorite movie is {favorite_movie}, it's a {genre} movie released in {release_year} , with a total duration of {duration_in_minutes} minutes, and a rating of {imdb_rating}" )

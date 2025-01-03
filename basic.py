3464909adba371d454dabb8cac989f2302db33ab
Izzy <ismail.dinia@gmail.com>
2025-01-03 18:41:06 +0100

    added lead actor

--- basic.py
+++ basic.py
@@ -5,5 +5,5 @@ genre = "Drama"
 release_year = 1994
 duration_in_minutes = 142
 imdb_rating = 9.3
-
+lead_actor = "Morgan Freeman"
 print(f" My favorite movie is {favorite_movie}, it's a {genre} movie released in {release_year} , with a total duration of {duration_in_minutes} minutes, and a rating of {imdb_rating}" )

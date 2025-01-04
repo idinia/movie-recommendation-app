eb805c4ee31f964a14107e35086ee64b3f5bdc64
Izzy <ismail.dinia@gmail.com>
2025-01-04 10:01:01 +0100

    syntax fixes and improving code

--- basic.py
+++ basic.py
@@ -6,4 +6,11 @@ release_year = 1994
 duration_in_minutes = 142
 imdb_rating = 9.3
 lead_actor = "Morgan Freeman"
+
 print(f" My favorite movie is {favorite_movie}, it's a {genre} movie released in {release_year} , with a total duration of {duration_in_minutes} minutes, and a rating of {imdb_rating}" )
+
+user_favorite_movie = input(" What is your favorite movie? ")
+user_genre = input( "What genre is it?")
+user_duration_in_minutes = input("How long is it? ")
+user_imdb_rating = input("What is its IMDB rating? ")
+user_lead_actor = input(" Who is the lead actor or actress? ")
